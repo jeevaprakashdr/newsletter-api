fae55244032dcbc705dc2c1b3fd93d70aff83c0a
jeeva <jeevaprakashdr@gmail.com>
2024-02-03 20:00:11 +0100

    added `subscriptions` endpoint and preliminary integration test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -3,12 +3,12 @@ use std::net::TcpListener;
 #[tokio::test]
 async fn health_check_succeed() {
     // Arrange
-    let url = spawn_app();
+    let app_address = spawn_app();
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get(format!("{}/health_check", url))
+        .get(format!("{}/health_check", app_address))
         .send()
         .await
         .expect("Health check failed");
@@ -16,6 +16,55 @@ async fn health_check_succeed() {
     // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length())
+}
+
+#[tokio::test]
+async fn subscribe_returns_200_ok_for_valid_form_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let body = "name=jk&email=newsletter-api%40gmail.com";
+
+    // Act
+    let response = client
+        .get(format!("{}/subscriptions", app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    // Assert
+    assert!(response.status().is_success());
+}
+
+#[tokio::test]
+async fn subscribe_returns_400_when_passed_with_invalid_form_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let invalid_data = vec![
+        ("name=jk", "missing email address"),
+        ("email=newsletter-api%40gmail.com", "missing name"),
+        ("", "missing name and email address"),
+    ];
+
+    // Act
+    for (body, error_message) in invalid_data {
+        let response = client
+            .get(format!("{}/subscriptions", app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API request did not failed with error message: {}",
+            error_message
+        );
+    }
 }
 
 fn spawn_app() -> String {
