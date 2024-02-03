3d3f6b6264296528dd3f27ada575bda7e287824b
jeeva <jeevaprakashdr@gmail.com>
2024-02-03 19:22:59 +0100

    Refactor: Bind random port with the help of TCPListener

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
+use std::net::TcpListener;
+
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    newsletter_api::run()?.await
+    let listener = TcpListener::bind("http://127.0.0.1:0").unwrap();
+    match newsletter_api::run(listener) {
+        Ok(_) => Ok(()),
+        Err(e) => return Err(e),
+    }
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,12 +1,14 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_succeed() {
     // Arrange
-    spawn_app();
+    let url = spawn_app();
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get("http://localhost:8080/health_check")
+        .get(format!("{}/health_check", url))
         .send()
         .await
         .expect("Health check failed");
@@ -16,8 +18,12 @@ async fn health_check_succeed() {
     assert_eq!(Some(0), response.content_length())
 }
 
-fn spawn_app() {
-    let server = newsletter_api::run().expect("Failed to spin the sever");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to server");
+    let port = listener.local_addr().unwrap().port();
 
+    println!("port {port}");
+    let server = newsletter_api::run(listener).expect("Failed to spin the sever");
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
