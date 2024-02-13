7bc91c9911f6926653b243a2f3179263a4b035f5
jeeva <jeevaprakashdr@gmail.com>
2024-02-13 13:15:12 +0100

    layman logging

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -14,12 +14,18 @@ pub async fn subscribe(
     form_data: web::Form<FormData>,
     connection: web::Data<PgPool>,
 ) -> HttpResponse {
+    let request_id = Uuid::new_v4();
     log::info!(
-        "Saving new subscriber details {}, {}",
-        form_data.name,
-        form_data.email
+        "request_id {} - Adding '{}' '{}' as a new subscriber.",
+        request_id,
+        form_data.email,
+        form_data.name
     );
 
+    log::info!(
+        "request_id {} - Saving new subscriber details in the database",
+        request_id
+    );
     match sqlx::query!(
         r#"INSERT INTO subscriptions(id, email, name, subscribed_at) VALUES($1, $2, $3, $4)"#,
         Uuid::new_v4(),
