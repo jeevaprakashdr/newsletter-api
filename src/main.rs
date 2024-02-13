ad09b123aed6a422b41f13b667e605cdaabefd02
jeeva <jeevaprakashdr@gmail.com>
2024-02-13 15:20:48 +0100

    adding log formatters

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use std::net::TcpListener;
 
-use env_logger::Env;
 use newsletter_api::configuration;
 use newsletter_api::startup::run;
 use sqlx::PgPool;
+use tracing::dispatcher::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    LogTracer::init().expect("Failed to initialize logger");
+    
     let settings = configuration::get_configuration().expect("Failed to read the configuration");
     let address = format!("127.0.0.1:{}", settings.application_port);
     let listener = TcpListener::bind(address)?;
@@ -14,6 +19,16 @@ async fn main() -> Result<(), std::io::Error> {
         .await
         .expect("failed to connect to database");
 
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let format_layer = BunyanFormattingLayer::new("newsletter".into(), std::io::stdout);
+
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(format_layer)
+        .into();
+
+    set_global_default(subscriber).expect("Failed to set tracing subscriber");
+
     run(listener, connection)?.await
 }
