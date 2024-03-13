cdc28c4905fb067ef99487c88a1ae29670046432
jeeva <jeevaprakashdr@gmail.com>
2024-03-13 22:27:41 +0100

    added failing test for having an link in confirmation  email

--- tests/subscriptions.rs
+++ tests/subscriptions.rs
@@ -53,27 +53,6 @@ async fn subscribe_returns_400_when_passed_with_invalid_form_data() {
 }
 
 #[tokio::test]
-async fn subscribe_sends_a_confirmation_email_for_valid_request() {
-    let app = spawn_app().await;
-    let body = "name=jk&email=newsletter-api%40gmail.com";
-
-    Mock::given(path("/email"))
-        .and(method("POST"))
-        .respond_with(ResponseTemplate::new(200))
-        .mount(&app.email_server)
-        .await;
-
-    // Act
-    let response = app
-        .post_subscription(body.into())
-        .await
-        .expect("Failed to execute request");
-
-    // Assert
-    // Mock asserts sending of email on Drop
-}
-
-#[tokio::test]
 async fn subscribe_persist_valid_form_data() {
     let app = spawn_app().await;
     let body = "name=jk&email=newsletter-api%40gmail.com";
@@ -101,5 +80,65 @@ async fn subscribe_persist_valid_form_data() {
     assert_eq!(saved.name, "jk");
     assert_eq!(saved.email, "newsletter-api@gmail.com");
     assert_eq!(saved.status, "pending-confirmation")
+}
 
+#[tokio::test]
+async fn subscribe_sends_a_confirmation_email_for_valid_request() {
+    let app = spawn_app().await;
+    let body = "name=jk&email=newsletter-api%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    // Act
+    app
+        .post_subscription(body.into())
+        .await
+        .expect("Failed to execute request");
+
+    // Assert
+    // Mock asserts sending of email on Drop
+}
+
+#[tokio::test]
+async fn subscribe_sends_a_confirmation_email_with_link() {
+    let app = spawn_app().await;
+    let body = "name=jk&email=newsletter-api%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    // Act
+    app
+        .post_subscription(body.into())
+        .await
+        .expect("Failed to execute request");
+
+    // Assert
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    let email_body: serde_json::Value = serde_json::from_slice(&email_request.body)
+        .unwrap();
+
+    let get_link = |s: &str| {
+        let links: Vec<_> = linkify::LinkFinder::new()
+            .links(s)
+            .filter(|l| *l.kind() == linkify::LinkKind::Url)
+            .collect();
+
+        assert_eq!(links.len(), 1);
+
+        links[0].as_str().to_owned()
+    };
+
+    let html_link = get_link(&email_body["HtmlContent"].as_str().unwrap());
+    let text_link = get_link(&email_body["TextContent"].as_str().unwrap());
+
+    assert_eq!(html_link, text_link)
 }
